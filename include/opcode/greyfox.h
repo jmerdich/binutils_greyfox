4ac5485301eda7f1cfe5726bb5a7b392eda382e6
Jake Merdich <jake@merdich.com>
2024-01-11 22:09:25 -0500

    First pass at disassembler

--- include/opcode/greyfox.h
+++ include/opcode/greyfox.h
@@ -16,8 +16,34 @@
    Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA
    02110-1301, USA.  */
 
+#define GREYFOX_OPC_TYPE_BAD                0x0
+#define GREYFOX_OPC_TYPE_NO_OPERAND         0x1
+#define GREYFOX_OPC_TYPE_ONE_OPERAND        0x2
+#define GREYFOX_OPC_TYPE_TWO_OPERAND        0x3
+#define GREYFOX_OPC_TYPE_THREE_OPERAND      0x4
+#define GREYFOX_OPC_TYPE_SHORT_BRANCH       0x5
+#define GREYFOX_OPC_TYPE_SVC                0x6
+#define GREYFOX_OPC_TYPE_HVC                0x7
+#define GREYFOX_OPC_TYPE_TWO_OPERAND_SAME   0x8
+
+#define GREYFOX_OPC_TYPEFLAG_A_IS_FLOAT     0x0001
+#define GREYFOX_OPC_TYPEFLAG_BC_IS_FLOAT    0x0002
+#define GREYFOX_OPC_TYPEFLAG_REGS_ARE_FLOAT 0x0003
+#define GREYFOX_OPC_TYPEFLAG_IMM_IS_FLOAT   0x0004
+#define GREYFOX_OPC_TYPEFLAG_ALL_FLOAT      0x0007
+#define GREYFOX_OPC_TYPEFLAG_A_IS_CP        0x0008
+#define GREYFOX_OPC_TYPEFLAG_IMM_IS_SIGNED  0x0010
+#define GREYFOX_OPC_TYPEFLAG_IMM_IS_PCREL   0x0020
+#define GREYFOX_OPC_TYPEFLAG_IMM_IS_ADDR    0x0040
+#define GREYFOX_OPC_TYPEFLAG_IMM_IS_RELADDR 0x0060
+
+
 typedef struct greyfox_opc_info_t
 {
-	short opcode;
+	short opcode;    /* opcode (always within 16b )*/
+	short mask;      /* mask of the opcode */
+	char  type;      /* type of the op */
+	char  len;       /* len of the op, including immediates */
+	short typeflags; /* typeflags of the op (can derive len from this) */
 	const char* name;
 } greyfox_opc_info_t;
--- opcodes/greyfox-dis.c
+++ opcodes/greyfox-dis.c
@@ -26,23 +26,148 @@
 #include "opcode/greyfox.h"
 #include "disassemble.h"
 
-extern const greyfox_opc_info_t greyfox_opc_info[128];
+extern const greyfox_opc_info_t greyfox_opc_info[];
 
 static fprintf_ftype fpr;
 static void* stream;
+
+bfd_signed_vma
+sign_extend(bfd_vma input, int bit);
+
+bfd_signed_vma
+sign_extend(bfd_vma input, int bit)
+{
+	return ((bfd_signed_vma)input << (64 - bit)) >> (64 - bit);
+}
 
 int
 print_insn_greyfox(bfd_vma addr, struct disassemble_info *info)
 {
 	int status;
 	stream = info->stream;
-	unsigned char opcode;
+	unsigned short opcode;
 	fpr = info->fprintf_func;
 
-	if ((status = info->read_memory_func(addr, &opcode, 1, info)))
+	if ((status = info->read_memory_func(addr, (bfd_byte*)&opcode, 2, info)))
 		goto fail;
 
-	fpr(stream, "%s", greyfox_opc_info[opcode].name);
+	const struct greyfox_opc_info_t* opci;
+
+	/* determine the opcode */
+	for (opci = greyfox_opc_info; opci->name; opci++)
+	{
+		if ((opcode & opci->mask) != opci->opcode) {
+			// wrong opcode
+			continue;
+		}
+		if ((opci->type == GREYFOX_OPC_TYPE_TWO_OPERAND_SAME) &&
+		    ((opcode & 0xF) != ((opcode >> 4) & 0xF))) {
+			// need "same" but this instruction has different
+			continue;
+		}
+
+		break; // Found the opcode! Last one is illegal.
+	}
+
+	// TODO: all sorts of type punning assumes little endian on this
+	union {
+		uint64_t u64;
+		int64_t i64;
+		double f64;
+		float f32;
+		bfd_byte bytes;
+	} immdata;
+	immdata.u64 = 0;
+	if (opci->len > 2) {
+		// Read the immediate if present. This must fail/pass before printing
+		if ((status = info->read_memory_func(addr + 2, &immdata.bytes, opci->len - 2, info)))
+			goto fail;
+	}
+
+	fpr(stream, "%s", opci->name);
+
+	// Print registers and args in the opcode itself (not trailing immediate)
+	bool immNeedsComma = false;
+	switch (opci->type) {
+		case GREYFOX_OPC_TYPE_BAD:
+		case GREYFOX_OPC_TYPE_NO_OPERAND:
+			break;
+		case GREYFOX_OPC_TYPE_ONE_OPERAND:
+		case GREYFOX_OPC_TYPE_TWO_OPERAND_SAME:
+			if (opci->typeflags & GREYFOX_OPC_TYPEFLAG_A_IS_FLOAT) {
+				fpr(stream, " f%d", opcode & 0xF);
+			} else if (opci->typeflags & GREYFOX_OPC_TYPEFLAG_A_IS_CP) {
+				fpr(stream, " cp_id%X", opcode & 0xF);
+			} else {
+				fpr(stream, " i%d", opcode & 0xF);
+			}
+			immNeedsComma = true;
+			break;
+		case GREYFOX_OPC_TYPE_TWO_OPERAND:
+			OPCODES_ASSERT(!(opci->typeflags & GREYFOX_OPC_TYPEFLAG_A_IS_CP));
+			fpr(stream,
+			    " %c%d, %c%d",
+			    (opci->typeflags & GREYFOX_OPC_TYPEFLAG_A_IS_FLOAT ? 'f' : 'i'),
+			    opcode & 0xF,
+			    (opci->typeflags & GREYFOX_OPC_TYPEFLAG_BC_IS_FLOAT ? 'f' : 'i'),
+			    (opcode >> 8) & 0xF);
+			immNeedsComma = true;
+			break;
+		case GREYFOX_OPC_TYPE_THREE_OPERAND:
+			OPCODES_ASSERT(opci->len == 2);
+			OPCODES_ASSERT(!(opci->typeflags & GREYFOX_OPC_TYPEFLAG_A_IS_CP));
+			fpr(stream,
+			    " %c%d, %c%d, %c%d",
+			    (opci->typeflags & GREYFOX_OPC_TYPEFLAG_A_IS_FLOAT ? 'f' : 'i'),
+			    opcode & 0xF,
+			    (opci->typeflags & GREYFOX_OPC_TYPEFLAG_BC_IS_FLOAT ? 'f' : 'i'),
+			    (opcode >> 8) & 0xF,
+			    (opci->typeflags & GREYFOX_OPC_TYPEFLAG_BC_IS_FLOAT ? 'f' : 'i'),
+			    (opcode >> 16) & 0xF);
+			break;
+		case GREYFOX_OPC_TYPE_SHORT_BRANCH:
+			OPCODES_ASSERT(opci->len == 2);
+			fpr(stream, " ");
+			info->print_address_func(addr + sign_extend(opcode << 1 & 0xFFF, 12), info);
+			break;
+		case GREYFOX_OPC_TYPE_SVC:
+		case GREYFOX_OPC_TYPE_HVC:
+			OPCODES_ASSERT(opci->len == 2);
+			// TODO: well-known hvc/svc calls
+			fpr(stream, " 0x%x", opcode & 0x3FF);
+			break;
+	}
+
+	if (opci->len > 2) {
+		if (immNeedsComma) {
+			fpr(stream, ",");
+		}
+
+		if (opci->typeflags & GREYFOX_OPC_TYPEFLAG_IMM_IS_ADDR) {
+			fpr(stream, " ");
+			OPCODES_ASSERT(!(opci->typeflags & GREYFOX_OPC_TYPEFLAG_IMM_IS_SIGNED));
+			if (opci->typeflags & GREYFOX_OPC_TYPEFLAG_IMM_IS_PCREL) {
+				immdata.u64 += addr;
+			}
+			info->print_address_func(immdata.u64, info);
+		} else if (opci->typeflags & GREYFOX_OPC_TYPEFLAG_IMM_IS_FLOAT) {
+			if (opci->len == 4) {
+				// TODO: human readable float16
+				fpr(stream, " 0x%04lx", immdata.u64);
+			} else if (opci->len == 6) {
+				fpr(stream, " %f", immdata.f32);
+			} else {
+				OPCODES_ASSERT(opci->len == 10);
+				fpr(stream, " %f", immdata.f64);
+			}
+		} else if (opci->typeflags & GREYFOX_OPC_TYPEFLAG_IMM_IS_SIGNED) {
+			immdata.i64 = sign_extend(immdata.u64, (opci->len - 2) * 8);
+			fpr(stream, " %ld", immdata.i64);
+		} else {
+			fpr(stream, " %lu", immdata.u64);
+		}
+	}
+
 
 	return 1;
 
